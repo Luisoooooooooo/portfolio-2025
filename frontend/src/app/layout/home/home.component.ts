eb72e8b53d16888f2af45e969c92bf0fb95b1cc3
Luisoooooooooo <luisalfonsovazquezsanchez@gmail.com>
2025-10-27 23:20:11 +0100

    Feature: Add GSAP animation to hero text

--- frontend/src/app/layout/home/home.component.ts
+++ frontend/src/app/layout/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import SplitText from 'gsap/SplitText';
+gsap.registerPlugin(ScrollTrigger, SplitText)
 
 @Component({
   selector: 'app-home',
@@ -6,6 +10,27 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private split?: SplitText;
 
+  ngOnInit() {
+    requestAnimationFrame(() => {
+      // selector correcto
+      this.split = new SplitText('.hero-text', { type: 'lines', linesClass: 'line' });
+
+      ScrollTrigger.refresh();
+
+      gsap.from(this.split.lines, {
+        yPercent: 120,
+        autoAlpha: 0,
+        stagger: 0.02,
+        duration: 1,
+        ease: 'power2.out'
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.split?.revert();
+  }
 }
